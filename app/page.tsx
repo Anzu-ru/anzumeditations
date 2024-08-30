c07ff68f66ee2e28c5eeafdef4601d6afc952679
Anzu-ru <flyingnarwhalgames@gmail.comgit config --global user.email flyingnarwhalgames@gmail.comgit config --global user.email flyingnarwhalgames@gmail.com>
2024-08-30 14:24:39 -0700

    Image Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:size-auto lg:bg-none">
-          
-          <h2 className="absolute left-[+10px] top-[-5] mt-6 text-gray-700">
-            Website Under Development
-          </h2>
+    
 
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center :absolute before:h-[300px] before:w-full before:-translate-x-1/2">
+      <div className="relative z-[-1] flex place-items-center :absolute">
         <Image
           className="relative"
           src="/logo.svg"
           alt="Anzu Meditations"
-          width={700}
-          height={200}
+          width={1000}
+          height={300}
           priority
         />
-      </div>
-
-      <div className="relative flex place-items-center">
-        <h1 className="relative flex mb-3 text-4xl font-bold">
-          Full Sensory Meditation
-        </h1>
       </div>
 
       
@@ -47,7 +32,7 @@ export default function Home() {
   Meditation Classes
 </h2>
 <h1 className="m-0 max-w-[30ch] text-3xl">
-$45/Session
+$35/Session
 </h1>
         </a>
 
@@ -55,29 +40,29 @@ $45/Session
 
         <a className="group rounded-lg border px-5 py-4 transition-colors">
 <h2 className="mb-2 text-2xl font-semibold">
-  60 Minute Sound Bath
+  30 Minute 1-on-1 Sound Bath
 </h2>
 <h1 className="m-0 max-w-[30ch] text-3xl">
-$120/Session
+$95/Session
 </h1>
         </a>
 
         <a className="group rounded-lg border px-5 py-4 transition-colors">
 <h2 className="mb-2 text-2xl font-semibold">
-  60 Minute 1-on-1 Full Sensory Session
+  30 Minute 1-on-1 Full Sensory Session
 </h2>
 <h1 className="m-0 max-w-[30ch] text-3xl">
-$150/Session
+$110/Session
 </h1>
         </a>
 
       </div>
 
       <div className="border-2 mb-32 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-3 lg:text-left px-5 py-4">
-        <h1 className="bold-40 lg:bold-64 xl:max-w-[390px] font-bold text-3xl mt-10">
-          Scolarships
+        <h1 className="bold-40 lg:bold-64 xl:max-w-[390px] font-bold text-3xl mt-3">
+        Scholarships
         </h1>
-        <p className="mt-5">I firmly believe that healing should not becost prohibative. In anattempt to help as many people aspossible, I offer free slots in my classes for those who cannot afford my standard rates. There are no strings attached. Email me at an@anzumeditations.com if you are interested!  </p>
+        <p className="mt-5">I firmly believe that healing should not be cost prohibative. In an attempt to help as many people as possible, I offer free slots in my classes for those who cannot afford my standard rates. There are no strings attached. Email me at an@anzumeditations.com if you are interested!  </p>
       </div>
 
   
